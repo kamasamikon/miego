09cdf183f124df09cde619bb51153e77c243b20a
kamasamikon <kamasamikon@qq.com>
2019-08-12 14:19:08 +0800

    misc: atoi and uintdatetime

--- misc/misc.go
+++ misc/misc.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"math/rand"
 	"runtime"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -45,64 +44,6 @@ func ReverseBytes(s []byte) []byte {
 	return s
 }
 
-// UintTime : convert time to 20060102150305
-func UintTime(t time.Time) uint64 {
-	nnnn := t.Year()
-	yy := t.Month()
-	rr := t.Day()
-	ss := t.Hour()
-	ff := t.Minute()
-	mm := t.Second()
-
-	s := fmt.Sprintf("%04d%02d%02d%02d%02d%02d", nnnn, yy, rr, ss, ff, mm)
-	res, _ := strconv.ParseUint(s, 0, 64)
-
-	return res
-}
-
-// UintTimeNow : convert time to 20060102150305
-func UintTimeNow() uint64 {
-	t := time.Now()
-
-	nnnn := t.Year()
-	yy := t.Month()
-	rr := t.Day()
-	ss := t.Hour()
-	ff := t.Minute()
-	mm := t.Second()
-
-	s := fmt.Sprintf("%04d%02d%02d%02d%02d%02d", nnnn, yy, rr, ss, ff, mm)
-	res, _ := strconv.ParseUint(s, 0, 64)
-
-	return res
-}
-
-// UintDate : convert time to 20060102
-func UintDate(t time.Time) uint {
-	nnnn := t.Year()
-	yy := t.Month()
-	rr := t.Day()
-
-	s := fmt.Sprintf("%04d%02d%02d", nnnn, yy, rr)
-	res, _ := strconv.ParseUint(s, 0, 64)
-
-	return uint(res)
-}
-
-// UintDateNow : convert time to 20060102
-func UintDateNow() uint {
-	t := time.Now()
-
-	nnnn := t.Year()
-	yy := t.Month()
-	rr := t.Day()
-
-	s := fmt.Sprintf("%04d%02d%02d", nnnn, yy, rr)
-	res, _ := strconv.ParseUint(s, 0, 64)
-
-	return uint(res)
-}
-
 func MemConsume() {
 	var c chan int
 	var wg sync.WaitGroup
@@ -129,15 +70,6 @@ func MemConsume() {
 
 	after := memConsumed() //获取创建goroutine后内存
 	fmt.Printf("%.3f KB\n", float64(after-before)/goroutineNum/1000)
-}
-
-// Atoi : atoi, if fail return default value
-func Atoi(a string, def int64) int64 {
-	x, e := strconv.ParseInt(a, 0, 64)
-	if e != nil {
-		return def
-	}
-	return x
 }
 
 // Epos : convert Excel Position to index, e.g. A .... AZ .... BC to 0, ... 16, ...
