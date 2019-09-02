7efeb977dcf585651c5602a43a288b86c3577595
kamasamikon <kamasamikon@qq.com>
2019-09-02 09:58:20 +0800

    xxx

--- misc/like.go
+++ misc/like.go
@@ -4,11 +4,16 @@ import (
 	"strings"
 )
 
-func KrLike(s string) (out string, isLike bool) {
+// KrLike : Convert to MySQL LIKE
+func KrLike(s string, fmtMode bool) (out string, isLike bool) {
 	var ss string
 
 	ss = strings.Replace(s, "*", "%", -1)
 	ss = strings.Replace(ss, ".", "_", -1)
+
+	if fmtMode {
+		ss = strings.Replace(ss, "%", "%%", -1)
+	}
 
 	if strings.IndexByte(ss, '%') >= 0 {
 		return ss, true
