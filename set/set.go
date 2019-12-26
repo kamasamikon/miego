95f471b1e1e0bd5ef2fad9d162764e5d7bb33202
kamasamikon <kamasamikon@qq.com>
2019-12-26 11:51:56 +0800

    set: add clr()

--- set/set.go
+++ set/set.go
@@ -9,17 +9,26 @@ func New() StrSet {
 func (s StrSet) Set(key string) {
 	s[key] = 1
 }
+
 func (s StrSet) Has(key string) bool {
 	_, ok := s[key]
 	return ok
 }
+
 func (s StrSet) Rem(key string) {
 	delete(s, key)
 }
+
 func (s StrSet) Keys() []string {
 	var key []string
 	for k, _ := range s {
 		key = append(key, k)
 	}
 	return key
+}
+
+func (s StrSet) Clr() {
+	for k := range s {
+		delete(s, k)
+	}
 }
