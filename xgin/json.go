22ff132f93ef5e163753a0d9ddd0833fe6e6a374
kamasamikon <kamasamikon@qq.com>
2020-01-09 02:10:15 +0800

    xxx

--- xgin/json.go
+++ xgin/json.go
@@ -1,24 +1,47 @@
 package xgin
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"runtime"
 )
 
 // JPong : JSON Pong
-type JPong struct {
+type PongBody struct {
 	Error   int         `json:"Error"`
 	Message string      `json:"Message"`
 	Data    interface{} `json:"Data"`
 }
 
-func J(c *gin.Context, Code int, Error int, Message string, Data interface{}) {
+func Pong(c *gin.Context, Code int, Error int, Message interface{}, Data interface{}) {
 	if Data == nil {
-		Data = &gin.H{}
+		if _, filename, line, ok := runtime.Caller(2); ok {
+			Data = fmt.Sprintf("%s:%d", filename, line)
+		} else {
+			Data = &gin.H{}
+		}
+	}
+
+	var Text string
+	if s, ok := Message.(error); ok {
+		Text = s.Error()
+	} else if s, ok := Message.(string); ok {
+		Text = s
+	} else {
+		Text = ""
 	}
 
 	c.JSON(Code, &gin.H{
 		"Error":   Error,
-		"Message": Message,
+		"Message": Text,
 		"Data":    Data,
 	})
+}
+
+func PongOK(c *gin.Context, Data interface{}) {
+	Pong(c, 200, 0, "", Data)
+}
+
+func PongNG(c *gin.Context, Code int, Error int, Message interface{}) {
+	Pong(c, Code, Error, Message, nil)
 }
