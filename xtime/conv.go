2e61088330b9ed9fded9609705a5a3035b581a08
kamasamikon <kamasamikon@qq.com>
2020-04-25 02:30:17 +0800

    xxx

--- xtime/conv.go
+++ xtime/conv.go
@@ -17,10 +17,17 @@ func NumToStr(o interface{}) string {
 
 	l := len(timestr)
 	if l == 14 {
+		// Second
 		if t, err := time.Parse("20060102150405", timestr); err == nil {
 			return t.Format("2006/01/02 15:04:05")
 		}
+	} else if l == 12 {
+		// Minute
+		if t, err := time.Parse("200601021504", timestr); err == nil {
+			return t.Format("2006/01/02 15:04")
+		}
 	} else if l == 8 {
+		// Day
 		if t, err := time.Parse("20060102", timestr); err == nil {
 			return t.Format("2006/01/02")
 		}
@@ -32,6 +39,9 @@ func NumToStr(o interface{}) string {
 func StrToNum(s string) uint64 {
 	if t, err := time.Parse("2006-01-02", s); err == nil {
 		return atox.Uint64(t.Format("20060102"), 0)
+	}
+	if t, err := time.Parse("2006-01-02 15:04", s); err == nil {
+		return atox.Uint64(t.Format("20060021504"), 0)
 	}
 	if t, err := time.Parse("2006-01-02 15:04:05", s); err == nil {
 		return atox.Uint64(t.Format("2006002150405"), 0)
