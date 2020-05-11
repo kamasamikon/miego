ac57e4e55080acf8afb0932377aab159b0af17f0
kamasamikon <kamasamikon@qq.com>
2020-05-11 21:22:08 +0800

    fill string to table

--- otot/objtotable.go
+++ otot/objtotable.go
@@ -20,3 +20,50 @@ func Convert(key string, val string, obj interface{}) string {
 
 	return strings.Join(lines, "\n")
 }
+
+func ViaKV(class string, id string, column int, v ...string) string {
+	var lines []string
+
+	lines = append(lines, fmt.Sprintf(`<table id="%s" class="%s table is-fullwidth">`, id, class))
+	lines = append(lines, "<tbody>")
+	for i := 0; i < len(v)/2; i++ {
+		if i%column == 0 {
+			if i != 0 {
+				lines = append(lines, "</tr>")
+			}
+			lines = append(lines, "<tr>")
+		}
+		k := v[2*i]
+		v := v[2*i+1]
+		lines = append(lines, fmt.Sprintf("<td>%s%s</td>", k, v))
+	}
+
+	lines = append(lines, "</tr>")
+	lines = append(lines, "</tbody>")
+	lines = append(lines, "</table>")
+
+	return strings.Join(lines, "\n")
+}
+
+func ViaS(class string, id string, column int, v ...string) string {
+	var lines []string
+
+	lines = append(lines, fmt.Sprintf(`<table id="%s" class="%s table is-fullwidth">`, id, class))
+	lines = append(lines, "<tbody>")
+	for i := 0; i < len(v); i++ {
+		if i%column == 0 {
+			if i != 0 {
+				lines = append(lines, "</tr>")
+			}
+			lines = append(lines, "<tr>")
+		}
+		s := v[i]
+		lines = append(lines, s)
+	}
+
+	lines = append(lines, "</tr>")
+	lines = append(lines, "</tbody>")
+	lines = append(lines, "</table>")
+
+	return strings.Join(lines, "\n")
+}
