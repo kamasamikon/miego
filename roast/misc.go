69ff70bf12757a6ce56a87830cd715fade1be44b
kamasamikon <kamasamikon@qq.com>
2021-01-17 10:15:59 +0800

    xx

--- roast/misc.go
+++ roast/misc.go
@@ -3,12 +3,12 @@ package roast
 import (
 	"github.com/kamasamikon/miego/xmap"
 	"github.com/kamasamikon/miego/xtime"
+	"github.com/kamasamikon/miego/xvx/nvn"
 )
 
-func NewAt__Str(p xmap.Map) {
-	p.Put("NewAt__Str", xtime.NumToStr(p.S("NewAt")))
+func Str__Time(mp xmap.Map, field string) {
+	mp.Put(field+"__Str", xtime.NumToStr(field))
 }
-
-func CrtAt__Str(p xmap.Map) {
-	p.Put("CrtAt__Str", xtime.NumToStr(p.S("CrtAt")))
+func Str__Nvn(mp xmap.Map, field string, nvnClass string) {
+	mp.Put(field+"__Str", nvn.S(field, nvnClass))
 }
