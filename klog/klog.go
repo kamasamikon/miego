12068f446933546bba910790abd671f9182f9ea0
kamasamikon <kamasamikon@qq.com>
2022-04-08 00:28:13 +0800

    xxx

--- klog/klog.go
+++ klog/klog.go
@@ -2,10 +2,6 @@ package klog
 
 import (
 	"fmt"
-	"path/filepath"
-	"runtime"
-	"strings"
-	"time"
 
 	"github.com/davecgh/go-spew/spew"
 )
@@ -24,115 +20,6 @@ var Conf struct {
 	ShortPath bool
 	NoColor   bool
 	Mute      bool
-}
-
-// BT : Print back trace
-func BT(maxdep int, formating string, args ...interface{}) string {
-	if Conf.Mute {
-		return ""
-	}
-
-	now := time.Now()
-	nowQ := now.Format("2006/01/02 15:04:05.")
-	nowH := now.Nanosecond() / 1000 / 1000 % 1000
-
-	cEnd := ColorType_Reset
-	cStart := ColorType_D
-
-	content := ""
-
-	txt := fmt.Sprintf(formating, args...)
-	content += fmt.Sprintf("%s|BT>>>>|%s %s\n", cStart, cEnd, txt)
-
-	dep := 0
-	for {
-		dep += 1
-
-		if maxdep > 0 && dep > maxdep {
-			break
-		}
-
-		pc, filename, line, ok := runtime.Caller(dep)
-		if ok == false {
-			content += fmt.Sprintf("%s|BT<<<<|%s %s\n", cStart, cEnd, txt)
-			return content
-		}
-
-		funcname := runtime.FuncForPC(pc).Name()
-		funcname = filepath.Ext(funcname)
-		funcname = strings.TrimPrefix(funcname, ".")
-
-		content += fmt.Sprintf("%s|BT|S:%s%03d|F:%s|H:%s|L:%d|%s\n", cStart, nowQ, nowH, filename, funcname, line, cEnd)
-	}
-
-	content += fmt.Sprintf("%s|BT<<<<|%s %s\n", cStart, cEnd, txt)
-	return content
-}
-
-// KLogLN : Log with CR
-func KLogLN(dep int, shortPath bool, color string, class string, formating string, args ...interface{}) {
-	if Conf.Mute {
-		return
-	}
-	if Conf.NoColor {
-		color = ""
-	}
-
-	filename, line, funcname := "???", 0, "???"
-	pc, filename, line, ok := runtime.Caller(dep)
-
-	if ok {
-		funcname = runtime.FuncForPC(pc).Name()
-		funcname = filepath.Ext(funcname)
-		funcname = strings.TrimPrefix(funcname, ".")
-	}
-
-	if shortPath {
-		filename = filepath.Base(filename)
-	}
-
-	cEnd := ColorType_Reset
-	if color == "" {
-		cEnd = ""
-	}
-
-	now := time.Now()
-	nowQ := now.Format("2006/01/02 15:04:05.")
-	nowH := now.Nanosecond() / 1000 / 1000 % 1000
-	fmt.Printf("%s|%s|S:%s%03d|F:%s|H:%s|L:%d|%s %s\n", color, class, nowQ, nowH, filename, funcname, line, cEnd, fmt.Sprintf(formating, args...))
-}
-
-// KLogX : No '\s' appended.
-func KLog(dep int, shortPath bool, color string, class string, formating string, args ...interface{}) {
-	if Conf.Mute {
-		return
-	}
-	if Conf.NoColor {
-		color = ""
-	}
-
-	filename, line, funcname := "???", 0, "???"
-	pc, filename, line, ok := runtime.Caller(dep)
-
-	if ok {
-		funcname = runtime.FuncForPC(pc).Name()
-		funcname = filepath.Ext(funcname)
-		funcname = strings.TrimPrefix(funcname, ".")
-	}
-
-	if shortPath {
-		filename = filepath.Base(filename)
-	}
-
-	cEnd := ColorType_Reset
-	if color == "" {
-		cEnd = ""
-	}
-
-	now := time.Now()
-	nowQ := now.Format("2006/01/02 15:04:05.")
-	nowH := now.Nanosecond() / 1000 / 1000 % 1000
-	fmt.Printf("%s|%s|S:%s%03d|F:%s|H:%s|L:%d|%s %s", color, class, nowQ, nowH, filename, funcname, line, cEnd, fmt.Sprintf(formating, args...))
 }
 
 // F :Fatal
