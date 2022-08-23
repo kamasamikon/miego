b6e2af23234d5b1dedba1ada7bd4b7667e4709ef
kamasamikon <kamasamikon@qq.com>
2022-08-23 17:39:45 +0800

    xxx

---
+++ roast/conf.go
@@ -0,0 +1,17 @@
+package roast
+
+import (
+	"github.com/kamasamikon/miego/conf"
+)
+
+var Conf struct {
+	Noisy            bool // call klog
+	NotifyQueryError bool // call wxcard
+}
+
+func init() {
+	conf.OnReady(func() {
+		Conf.Noisy = conf.Bool(false, "b:/roast/Noisy")
+		Conf.NotifyQueryError = conf.Bool(false, "b:/roast/NotifyQueryError")
+	})
+}
