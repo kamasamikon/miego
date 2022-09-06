8288065d5a9947147370774bffe152821193a35a
kamasamikon <kamasamikon@qq.com>
2022-09-06 11:19:38 +0800

    xxx

--- ms/msb/deploy.sh
+++ ms/msb/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# REMOTE=root@106.75.49.211
 REMOTE=root@117.50.95.113
+REMOTE=root@106.75.49.211
 
 sudo docker save -o msb.tar msb
 
+sudo rm msb.tar.gz
 sudo chmod a+rwx msb.tar
 sudo gzip --best -c msb.tar >> msb.tar.gz
 sudo scp msb.tar.gz $REMOTE:/tmp/msb.tar.gz
@@ -17,5 +18,10 @@ ssh $REMOTE "gunzip /tmp/msb.tar.gz"
 ssh $REMOTE "docker load -i /tmp/msb.tar"
 
 echo ">>>>>>>>>>>>>>" ssh $REMOTE "docker run -d -it --restart=always --name msb -p 9080:80 msb"
-ssh $REMOTE "docker run -d -it --restart=always --name msb -p 9080:80 -p 9443:443 msb"
+
+ssh $REMOTE "docker rm -f msb"
+ssh $REMOTE "docker run -d -it --restart=always --name msb -p 9080:80 msb"
+
+ssh $REMOTE "docker rm -f msb.mp"
+ssh $REMOTE "docker run -d -it --restart=always --name msb.mp -p 9081:80 msb"
 
