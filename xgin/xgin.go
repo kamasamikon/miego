72549aebd6651e0a0608b9d8b0256aab37453709
kamasamikon <kamasamikon@qq.com>
2026-09-09 15:28:58 +0800

    xxx

--- xgin/xgin.go
+++ xgin/xgin.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -18,6 +20,16 @@ import (
 
 // Default :Only and default Engine
 var _Default *gin.Engine
+
+func IsPortAvailable(port int) bool {
+	addr := fmt.Sprintf(":%d", port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return false
+	}
+	listener.Close()
+	return true
+}
 
 func GetFreePort() (int, error) {
 	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
@@ -84,6 +96,51 @@ func RoutersToConf(Engine *gin.Engine) {
 	}
 }
 
+// ParseAddress 解析 "IP:Port?" 格式的字符串
+// 支持：IP可选、端口可选、问号可选
+func ParseAddress(s string) (ip string, port int, q bool, e error) {
+	if strings.HasSuffix(s, "?") {
+		q = true
+		s = strings.TrimSuffix(s, "?")
+	}
+
+	segs := strings.Split(s, ":")
+	if len(segs) != 2 {
+		e = fmt.Errorf("Bad address: '%s'", s)
+		return
+	}
+
+	ipPart := segs[0]
+	portPart := segs[1]
+
+	// 解析IP部分（可能为空）
+	if ipPart != "" {
+		if tmp := net.ParseIP(ipPart); tmp != nil {
+			ip = ipPart
+		} else {
+			e = fmt.Errorf("无效的IP地址: %s", ipPart)
+			return
+		}
+	}
+
+	// 解析端口部分（可能为空）
+	if portPart != "" {
+		xport, err := strconv.Atoi(portPart)
+		if err != nil {
+			e = fmt.Errorf("无效的端口号: %s", portPart)
+			return
+		}
+		if xport < 0 || xport > 65535 {
+			e = fmt.Errorf("端口号超出范围: %d (应为 0-65535)", xport)
+			return
+		}
+		port = xport
+		return
+	}
+
+	return
+}
+
 func Go(
 	Engine *gin.Engine,
 	addr string,
@@ -94,15 +151,29 @@ func Go(
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	if addr == "" {
-		addr = fmt.Sprintf(":%d", conf.I("ms/port", 8888))
-	}
-	if addr == "*" {
-		if port, err := GetFreePort(); err != nil {
-			return err
+	// addr == ":" => default
+	// addr == ":?" => random
+	// addr == ":NNN?" => NNN or random
+	if ip, port, q, err := ParseAddress(addr); err == nil {
+		if q {
+			if port == 0 {
+				// ...:? => GetFreePort
+				port, _ = GetFreePort()
+			} else {
+				// ...:NNN? => NNN ? GetFreePort
+				if !IsPortAvailable(port) {
+					port, _ = GetFreePort()
+				}
+			}
 		} else {
-			addr = fmt.Sprintf(":%d", port)
+			if port == 0 {
+				// ...: => Load from conf
+				port = int(conf.I("ms/port", 8888))
+			}
 		}
+		addr = fmt.Sprintf("%s:%d", ip, port)
+	} else {
+		return err
 	}
 
 	conf.SSetf("gin/addr", addr)
