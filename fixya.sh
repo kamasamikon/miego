617f7d6680ace12610d0292712ae7af16fbebca8
kamasamikon <kamasamikon@qq.com>
2026-09-15 20:51:01 +0800

    xxx

--- fixya.sh
+++ fixya.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo chmod a+w .git/logs/HEAD .git/logs/refs/heads/master .git/logs/refs/remotes/ee/master .git/logs/refs/remotes/origin/master
+sudo find . -type f -exec chmod a+w {} +
