1220b596a38d21e1c9c641fc176179a93cf78f8a
kamasamikon <kamasamikon@qq.com>
2026-09-16 20:39:15 +0800

    xxx

--- fixya.sh
+++ fixya.sh
@@ -3,4 +3,4 @@
 
 # find .git \( -name objects -o -name hooks -o -name __pycache__ \) -prune -o -type f -exec chmod a+w {} +
 
-find .git ! -path ".git/objects/*" ! -path ".git/logs/*" ! -path ".git/hooks/*" -type f -exec echo chmod a+x {} +
+find .git ! -path ".git/objects/*" ! -path ".git/logs/*" ! -path ".git/hooks/*" -type f -exec chmod a+x {} +
